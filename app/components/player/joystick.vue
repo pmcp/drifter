a284838c768b724043429a4e9e44d77cf05bd369
pmcp <github@maartenlauwaert.eu>
2024-10-10 11:31:34 +0200

    fixed joystick

--- app/components/player/joystick.vue
+++ app/components/player/joystick.vue
@@ -8,38 +8,25 @@
         :max="100"
         v-model="joystickValue"
 
-        @change="start"
+        @change="stopRunning"
        />
     </div>
   </div>
 </template>
 <script setup>
-const {
-  player,
-  playerIsPlaying,
-  playerIsReady,
-  playerCurrentTime,
-  playerTotalDuration,
 
-} = usePlayer()
-
+const { player, playerIsPlaying, playerIsReady, playerCurrentTime, playerTotalDuration } = usePlayer()
 const { joystickValue, moveJoystick } = useJoystick()
 const { isActive, pause, resume } = useIntervalFn(() => moveJoystick(), 10, {immediate: false})
 
-const stopRunning= () => {
-  start
-}
+const stopRunning= () => { pause(); joystickValue.value = 0; }
 
-const { isPending, start, stop } = useTimeoutFn(() => {
-  joystickValue.value = 0;
-}, 10, {immediate: false})
-
+joystickValue.value = 0;
 
 onMounted(() => {
-  joystickValue.value = 0;
   watch(joystickValue, async (newValue, oldValue) => {
     if(isActive.value) return;
-    // resume()
+    resume()
   })
 });
 
