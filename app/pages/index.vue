80ef351fc60ca4285f1ee1193e81a8723b5ebb11
pmcp <github@maartenlauwaert.eu>
2024-10-14 19:34:50 +0200

    changed color

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -18,35 +18,37 @@
   </div>
 
   <div class="pointer-events-none absolute bottom-4 left-0 w-full p-4 flex flex-col items-center ">
-    <UCard class="max-w-[60rem] pointer-events-auto">
+    <UCard class="max-w-[60rem] pointer-events-auto w-full">
       <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
         <div class="col-span-3 sm:col-span-3">
           <player-wavesurfer />
         </div>
 
         <div class="col-span-1 flex flex-row gap-2 grid-cols-1">
-          <items-add v-for="type in types" :key="type.id" :type="type">
-            {{ type.title}}
+          <items-add v-for="type in types.filter(x => (x.id === 'sample' || x.id === 'note'))" :key="type.id" :type="type">
+<!--            {{ type.title}}-->
           </items-add>
         </div>
 
 
 
         <div class="col-span-1 justify-self-center flex flex-row gap-2 items-center justify-center">
+<!--          <player-controls-zoom direction="-"/>-->
           <player-controls-in-out>
             <player-controls-play-pause />
           </player-controls-in-out>
+<!--          <player-controls-zoom direction="+"/>-->
         </div>
 
-        <div class="col-span-1 hidden md:block justify-self-center self-center">
+        <div class="col-span-1 text-xs md:block justify-self-center self-center">
           <player-duration />
         </div>
 
-        <div class="flex flex-row gap-2 items-center justify-center col-span-3">
-          <player-controls-zoom direction="-"/>
-          <player-joystick  class="flex-grow"/>
-          <player-controls-zoom direction="+"/>
-        </div>
+<!--        <div class="flex flex-row gap-2 items-center justify-center col-span-3">-->
+<!--&lt;!&ndash;          <player-controls-zoom direction="-"/>&ndash;&gt;-->
+<!--&lt;!&ndash;          <player-joystick  class="flex-grow"/>&ndash;&gt;-->
+
+<!--        </div>-->
       </div>
 
     </UCard>
